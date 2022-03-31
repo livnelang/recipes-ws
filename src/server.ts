8c5880c9bbe718a1dea6602841b7e03d81bc879e
Livne Lang <livnelang@gmail.com>
2022-03-31 13:51:59 +0300

    port change

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,7 @@ import  { Application} from "express";
 import express = require("express");
 import routes from './routes/routes';
 
-
 const app: Application = express();
-const port = 3000;
 
 // Body parsing Middleware
 app.use(express.json());
@@ -25,24 +23,6 @@ app.use(cors(options));
 // Addong routes
 app.use(routes);
 
-interface A {
-    name: string;
-    age: number;
-}
-
-const g: A = {
-    name: "aasdasdsdsad",
-    age: 343
-}
-
-console.log(g);
-
-// app.get("/", async (req: Request, res: Response): Promise<Response> => {
-//   return res.status(200).send({
-//     message: "Hello World!",
-//   });
-// });
-
-app.listen(port, (): void => {
-  console.log(`Connected successfully on port ${port}`);
+app.listen(process.env.PORT || 5000, (): void => {
+  console.log(`Connected successfully on port ${process.env.PORT}`);
 });
